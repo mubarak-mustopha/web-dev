4b86e261fd2debc5eef1e4148096aa2b1d77d7f5
mubarak-mustopha <mustophamubarack17@gmail.com>
2022-12-30 03:09:22 +0100

    Now imports function that creates the header section

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,63 +1,20 @@
-import Logo from "./images/logo.jpeg";
+import createHeader from "./header";
 import createHomePage from "./home";
 import "./style.css";
 
 
-let header = document.createElement("header");
+let header = createHeader();
+let main = document.createElement("main");
+let footer = document.createElement("footer");
 
-function createLogo() {
-    let logoDiv = document.createElement("div");
-    logoDiv.classList.add("logo");
 
-    let logoImg = new Image();
-    logoImg.src = Logo;
-    logoDiv.appendChild(logoImg);
 
-    let logoText = document.createElement("span");
-    logoText.textContent = "Muby's Doughnot";
-    logoDiv.appendChild(logoText);
-    return logoDiv
-}
-
-function createNav() {
-    let navList = document.createElement("ul");
-    navList.classList.add("nav");
-
-    //home list
-    let homeLi = document.createElement("li");
-    homeLi.setAttribute("id", "home");
-    let homeLink = createLinkTag("Home", "#");
-    homeLi.appendChild(homeLink);
-
-    //menu list
-    let menuLi = document.createElement("li");
-    menuLi.setAttribute("id", "menu");
-    let menuLink = createLinkTag("Menu", "#");
-    menuLi.appendChild(menuLink);
-
-    //Contact list
-    let contactLi = document.createElement("li");
-    contactLi.setAttribute("id", "contact");
-    let contactLink = createLinkTag("Contact", "#");
-    contactLi.appendChild(contactLink);
-
-    navList.appendChild(homeLi);
-    navList.appendChild(menuLi);
-    navList.appendChild(contactLi)
-
-    return navList;
-}
-
-function createLinkTag(text, href) {
-    let link = document.createElement("a");
-    link.textContent = text;
-    link.href = href;
-    return link
-}
-
-//append to header
-header.append(createLogo());
-header.append(createNav());
 
 //append header to dom
 document.body.appendChild(header);
+
+//append to main
+main.appendChild(createHomePage(main));
+
+//append main to dom
+document.body.appendChild(main)
