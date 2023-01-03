3a60c0bc27c380141fc738c2575a754c07cf61b6
mubarak-mustopha <mustophamubarack17@gmail.com>
2023-01-03 03:36:54 +0100

    Lines added to dynamically determine the main content of the page based on user click

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -16,8 +16,24 @@ let footer = createFooter();
 //append header to dom
 document.body.appendChild(header);
 
+
 //append to main
-main.appendChild(createContactPage(main));
+let homeLink = document.querySelector("#home");
+let menuLink = document.querySelector("#menu");
+let contactLink = document.querySelector("#contact");
+
+homeLink.addEventListener("click", () => {
+    main.appendChild(createHomePage(main));
+})
+
+menuLink.addEventListener("click", () => {
+    main.appendChild(createMenuPage(main))
+})
+
+contactLink.addEventListener("click", () => {
+    main.appendChild(createContactPage(main))
+})
+main.appendChild(createHomePage(main));
 
 //append main to dom
 document.body.appendChild(main)
