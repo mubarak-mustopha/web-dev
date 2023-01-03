5c871188c5dd35f127c545cbb4de00131177bc76
mubarak-mustopha <mustophamubarack17@gmail.com>
2023-01-03 06:21:50 +0100

    Container div added5

--- to-do-list/webpack.config.js
+++ to-do-list/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/dom.js',
     mode: "development",
     devtool: 'inline-source-map',
     output: {
